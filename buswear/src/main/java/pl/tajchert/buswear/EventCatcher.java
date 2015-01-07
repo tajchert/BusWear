49588080ba320795b46b0848825bcb179bac097b
Michal Tajchert <michal.tajchert@polidea.com>
2015-01-07 20:55:40 +0100

    EventCatcher added to the lib.

--- buswear/src/main/java/pl/tajchert/buswear/EventCatcher.java
+++ buswear/src/main/java/pl/tajchert/buswear/EventCatcher.java
@@ -1,12 +1,10 @@
-package pl.tajchert.buswear.sample;
+package pl.tajchert.buswear;
 
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
-import pl.tajchert.buswear.EventBus;
 
-
-public class MessagesCatcher extends WearableListenerService {
+public class EventCatcher extends WearableListenerService {
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
